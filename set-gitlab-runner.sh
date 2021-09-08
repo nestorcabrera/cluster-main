5d0386f022e392a9e86fc415e5ebe5d44c05412e
Nestor Cabrera <nic.net9@gmail.com>
2021-09-08 14:54:49 -0500

    gitlab runner ready

--- set-gitlab-runner.sh
+++ set-gitlab-runner.sh
@@ -1,6 +1,10 @@
-helm upgrade --install gitlab-runner-gnapp gitlab/gitlab-runner \
-    --namespace test \
-    --timeout 600s \
-    --version 0.32.0 \
-    --values values/gitlab-runner-gnapp.yaml \
-    --create-namespace
+#helm upgrade --install gitlab-runner-gnapp gitlab/gitlab-runner \
+#--namespace test \
+#--set gitlabUrl=https://gitlab.gnappv2.dev.gasneiva.com/ \
+#--set runnerRegistrationToken=SkpEh-svMmDCthSHQ3_6 \
+#--set rbac.create=true \
+#--set tags="kubernetes_gnapp" \
+#--set privileged=true \
+#--create-namespace
+
+helm upgrade --install --namespace test gitlab-runner-gnapp -f values/runner-chart-values.yaml gitlab/gitlab-runner
