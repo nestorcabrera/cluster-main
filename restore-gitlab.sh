a5713c9b109ba573f7cebcea7c9825e5c7095329
Nestor Cabrera <nic.net9@gmail.com>
2021-09-09 16:11:18 -0500

    ajuste gitlab-restore.sh

--- restore-gitlab.sh
+++ restore-gitlab.sh
@@ -1,9 +1,9 @@
 kubectl -n gitlab delete secret gitlab-rails-secret
 kubectl -n gitlab create secret generic <rails-secret-name> --from-file=secrets.yml=secrets.yml
 
-kubectl delete pods -lapp=sidekiq,release=gitlab
-kubectl delete pods -lapp=webservice,release=gitlab
-kubectl delete pods -lapp=task-runner,release=gitlab
+kubectl -n gitlab delete pods -lapp=sidekiq,release=gitlab
+kubectl -n gitlab delete pods -lapp=webservice,release=gitlab
+kubectl -n gitlab delete pods -lapp=task-runner,release=gitlab
 
 gitlab_task=gitlab-task-runner-57499c696c-8v6dh
 GS=gs:1631203153_2021_09_09_14.2.1
