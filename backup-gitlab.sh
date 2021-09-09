744af680365610e936bed4f19e5c2554fdd94f3f
Nestor Cabrera <nic.net9@gmail.com>
2021-09-09 14:24:35 -0500

    gitlab backup restore

--- backup-gitlab.sh
+++ backup-gitlab.sh
@@ -1,2 +1,2 @@
-export gitlab_task=gitlab-task-runner-57499c696c-8v6dh
-kubectl -n gitlab exec gitlab_task -it -- backup-utility
+gitlab_task=gitlab-task-runner-57499c696c-8v6dh
+kubectl -n gitlab exec $gitlab_task -it -- backup-utility
--- restore-gitlab.sh
+++ restore-gitlab.sh
@@ -1,3 +1,3 @@
-export gitlab_task=gitlab-task-runner-57499c696c-8v6dh
-export GS=gs://dev-gitlab-backup-storage/1631203153_2021_09_09_14.2.1_gitlab_backup.tar
-kubectl exec gitlab_task -it -- backup-utility --restore -t GS
+gitlab_task=gitlab-task-runner-57499c696c-8v6dh
+GS=gs://dev-gitlab-backup-storage/1631203153_2021_09_09_14.2.1_gitlab_backup.tar
+kubectl exec $gitlab_task -it -- backup-utility --restore -t $GS
