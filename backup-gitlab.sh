a42fe814618b27b50783864a8310c403c5bf120f
Nestor Cabrera <nic.net9@gmail.com>
2021-09-09 15:43:52 -0500

    gitlab ajustes backup

--- backup-gitlab.sh
+++ backup-gitlab.sh
@@ -1,2 +1,4 @@
+kubectl -n gitlab get secrets gitlab-rails-secret -o jsonpath="{.data['secrets\.yml']}" | base64 --decode > secrets.yaml
+
 gitlab_task=gitlab-task-runner-57499c696c-8v6dh
 kubectl -n gitlab exec $gitlab_task -it -- backup-utility
--- restore-gitlab.sh
+++ restore-gitlab.sh
@@ -1,3 +1,10 @@
+kubectl -n gitlab delete secret gitlab-rails-secret
+kubectl -n gitlab create secret generic <rails-secret-name> --from-file=secrets.yml=secrets.yml
+
+kubectl delete pods -lapp=sidekiq,release=gitlab
+kubectl delete pods -lapp=webservice,release=gitlab
+kubectl delete pods -lapp=task-runner,release=gitlab
+
 gitlab_task=gitlab-task-runner-57499c696c-8v6dh
-GS=gs://dev-gitlab-backup-storage/1631203153_2021_09_09_14.2.1_gitlab_backup.tar
-kubectl exec $gitlab_task -it -- backup-utility --restore -t $GS
+GS=gs:1631203153_2021_09_09_14.2.1
+kubectl -n gitlab exec $gitlab_task -it -- backup-utility --restore -t $GS
